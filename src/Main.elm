f643ecbc1211465d82a2f228209f5e90887ca212
Lassi Kortela <lassi@lassi.io>
2019-02-17 12:26:52 +0200

    Add key select and speak checkbox

--- src/Main.elm
+++ src/Main.elm
@@ -1,24 +1,47 @@
 module Main exposing (Model, Msg(..), init, main, subscriptions, update, view)
 
+import Array
 import Browser
 import Debug
 import Dict
-import Html
+import Html exposing (div, input, label, option, select, span, text)
+import Html.Attributes exposing (selected, type_, value)
+import Html.Events exposing (onClick, onInput)
 import Midi
 import Speech
+import Util
 import WebMidi
 
 
 type alias Model =
-    Midi.Message
+    { tonic : Int, speak : Bool, message : Midi.Message }
 
 
 type Msg
-    = MidiMessage WebMidi.Message
+    = SetTonic String
+    | ToggleSpeak
+    | MidiMessage WebMidi.Message
 
 
 type Scale
     = Scale (List Int) Int
+
+
+noteNames =
+    Array.fromList
+        [ "C"
+        , "Db"
+        , "D"
+        , "Eb"
+        , "E"
+        , "F"
+        , "F#"
+        , "G"
+        , "Ab"
+        , "A"
+        , "Bb"
+        , "B"
+        ]
 
 
 minorIntervals =
@@ -50,7 +73,7 @@ semitoneToScaleDegree scale semitone =
 
 init : () -> ( Model, Cmd Msg )
 init flags =
-    ( Midi.Unknown, Cmd.none )
+    ( { tonic = 0, speak = False, message = Midi.Unknown }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -58,36 +81,80 @@ subscriptions model =
     WebMidi.onMidiMessage MidiMessage
 
 
-fMinor =
-    Scale minorIntervals 5
+speechCommand model =
+    if not model.speak then
+        Cmd.none
 
+    else
+        case model.message of
+            Midi.NoteOn pitch ->
+                let
+                    scale =
+                        Scale minorIntervals model.tonic
+                in
+                case semitoneToScaleDegree scale (modBy 12 pitch) of
+                    Just scaleDegree ->
+                        Speech.speak (String.fromInt (1 + scaleDegree))
 
-speechCommand message =
-    case message of
-        Midi.NoteOn pitch ->
-            case semitoneToScaleDegree fMinor (modBy 12 pitch) of
-                Just scaleDegree ->
-                    Speech.speak (String.fromInt (1 + scaleDegree))
+                    Nothing ->
+                        Cmd.none
 
-                Nothing ->
-                    Cmd.none
-
-        _ ->
-            Cmd.none
+            _ ->
+                Cmd.none
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update (MidiMessage webMidiMessage) model =
-    let
-        message =
-            Midi.parseMessage webMidiMessage.midiEvent
-    in
-    ( message, speechCommand message )
+update msg model =
+    case msg of
+        SetTonic tonic ->
+            ( { model | tonic = Util.indexDefault tonic model.tonic }
+            , Cmd.none
+            )
+
+        ToggleSpeak ->
+            ( { model | speak = not model.speak }
+            , Cmd.none
+            )
+
+        MidiMessage webMidiMessage ->
+            let
+                message =
+                    Midi.parseMessage webMidiMessage.midiEvent
+
+                newModel =
+                    { model | message = message }
+            in
+            ( newModel, speechCommand newModel )
+
+
+noteOptions selectedIndex =
+    Array.indexedMap
+        (\index noteName ->
+            option
+                ([ value (String.fromInt index) ]
+                    ++ (if index == selectedIndex then
+                            [ selected True ]
+
+                        else
+                            []
+                       )
+                )
+                [ text noteName ]
+        )
+        noteNames
+        |> Array.toList
 
 
 view : Model -> Html.Html Msg
 view model =
-    model |> Debug.toString |> Html.text
+    div []
+        [ text "Key: "
+        , select [ onInput SetTonic ] (noteOptions model.tonic)
+        , label []
+            [ input [ type_ "checkbox", onClick ToggleSpeak ] []
+            , text "Speak"
+            ]
+        ]
 
 
 main : Program () Model Msg
